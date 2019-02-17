fc5b328c3e64f89a6f76cb6864ffecd0c3ba8a5e
Kyle Hammond <kyle.hammond@gmail.com>
2019-02-17 10:41:36 -0600

    Add base entity

---
+++ KSH.Babbages.Data/KSH.Babbages.Data/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KSH.Babbages.Entities
+{
+    internal class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Modified { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool IsSoftDeleted { get; set; }
+    }
+}
